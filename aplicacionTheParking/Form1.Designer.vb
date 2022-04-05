1485388cd4f8702c4b75faa7a51bb098b7f75886
Johan Daza <ferneydaza009@gmail.com>
2022-04-05 15:33:24 -0500

    Agregar archivos de proyecto.

---
+++ aplicacionTheParking/Form1.Designer.vb
@@ -0,0 +1,332 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtSi = New System.Windows.Forms.CheckBox()
+        Me.txtNo = New System.Windows.Forms.CheckBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextValor = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextIva = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextPaga = New System.Windows.Forms.TextBox()
+        Me.TextDevolver = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextPlaca = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2.SuspendLayout()
+        Me.Panel4.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"2 LLANTAS", "4 LLANTAS", "8 LLANTAS"})
+        Me.ComboBox1.Location = New System.Drawing.Point(50, 151)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 25)
+        Me.ComboBox1.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI Symbol", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label2.Location = New System.Drawing.Point(29, 15)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(141, 21)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Tipo de Vehiculo"
+        '
+        'txtSi
+        '
+        Me.txtSi.AutoSize = True
+        Me.txtSi.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.txtSi.Location = New System.Drawing.Point(293, 21)
+        Me.txtSi.Name = "txtSi"
+        Me.txtSi.Size = New System.Drawing.Size(43, 22)
+        Me.txtSi.TabIndex = 4
+        Me.txtSi.Text = "SI"
+        Me.txtSi.UseVisualStyleBackColor = True
+        '
+        'txtNo
+        '
+        Me.txtNo.AutoSize = True
+        Me.txtNo.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.txtNo.Location = New System.Drawing.Point(293, 42)
+        Me.txtNo.Name = "txtNo"
+        Me.txtNo.Size = New System.Drawing.Size(52, 22)
+        Me.txtNo.TabIndex = 5
+        Me.txtNo.Text = "NO"
+        Me.txtNo.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI Symbol", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label3.Location = New System.Drawing.Point(222, 20)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(45, 21)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "IVA*"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI Symbol", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label4.Location = New System.Drawing.Point(37, 165)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(66, 30)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Valor"
+        '
+        'TextValor
+        '
+        Me.TextValor.Cursor = System.Windows.Forms.Cursors.No
+        Me.TextValor.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TextValor.Location = New System.Drawing.Point(126, 279)
+        Me.TextValor.Name = "TextValor"
+        Me.TextValor.Size = New System.Drawing.Size(153, 29)
+        Me.TextValor.TabIndex = 8
+        '
+        'Button1
+        '
+        Me.Button1.BackgroundImage = CType(resources.GetObject("Button1.BackgroundImage"), System.Drawing.Image)
+        Me.Button1.Location = New System.Drawing.Point(100, 19)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(89, 40)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "Resultado "
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextIva
+        '
+        Me.TextIva.Cursor = System.Windows.Forms.Cursors.No
+        Me.TextIva.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TextIva.Location = New System.Drawing.Point(126, 238)
+        Me.TextIva.Name = "TextIva"
+        Me.TextIva.Size = New System.Drawing.Size(153, 29)
+        Me.TextIva.TabIndex = 10
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI Symbol", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label5.Location = New System.Drawing.Point(16, 128)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(92, 21)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Valor IVA*"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Segoe UI Symbol", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label6.Location = New System.Drawing.Point(21, 83)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(87, 21)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "Pago con "
+        '
+        'TextPaga
+        '
+        Me.TextPaga.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TextPaga.Location = New System.Drawing.Point(126, 193)
+        Me.TextPaga.Name = "TextPaga"
+        Me.TextPaga.Size = New System.Drawing.Size(153, 29)
+        Me.TextPaga.TabIndex = 12
+        '
+        'TextDevolver
+        '
+        Me.TextDevolver.Cursor = System.Windows.Forms.Cursors.No
+        Me.TextDevolver.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TextDevolver.Location = New System.Drawing.Point(126, 324)
+        Me.TextDevolver.Name = "TextDevolver"
+        Me.TextDevolver.Size = New System.Drawing.Size(153, 29)
+        Me.TextDevolver.TabIndex = 15
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Segoe UI Symbol", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label7.Location = New System.Drawing.Point(29, 214)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(79, 21)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "Devuelta"
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.Khaki
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel2.Controls.Add(Me.Label2)
+        Me.Panel2.Controls.Add(Me.Label7)
+        Me.Panel2.Controls.Add(Me.Label3)
+        Me.Panel2.Controls.Add(Me.Label6)
+        Me.Panel2.Controls.Add(Me.Label5)
+        Me.Panel2.Controls.Add(Me.txtNo)
+        Me.Panel2.Controls.Add(Me.Label4)
+        Me.Panel2.Controls.Add(Me.txtSi)
+        Me.Panel2.Controls.Add(Me.Panel3)
+        Me.Panel2.Font = New System.Drawing.Font("Arial Rounded MT Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Panel2.Location = New System.Drawing.Point(12, 112)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(435, 261)
+        Me.Panel2.TabIndex = 17
+        '
+        'Panel3
+        '
+        Me.Panel3.BackgroundImage = CType(resources.GetObject("Panel3.BackgroundImage"), System.Drawing.Image)
+        Me.Panel3.Location = New System.Drawing.Point(319, 112)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(82, 83)
+        Me.Panel3.TabIndex = 15
+        '
+        'Panel4
+        '
+        Me.Panel4.BackgroundImage = CType(resources.GetObject("Panel4.BackgroundImage"), System.Drawing.Image)
+        Me.Panel4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel4.Controls.Add(Me.Button2)
+        Me.Panel4.Controls.Add(Me.Button1)
+        Me.Panel4.Location = New System.Drawing.Point(12, 380)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(435, 82)
+        Me.Panel4.TabIndex = 18
+        '
+        'Button2
+        '
+        Me.Button2.BackgroundImage = CType(resources.GetObject("Button2.BackgroundImage"), System.Drawing.Image)
+        Me.Button2.Location = New System.Drawing.Point(195, 19)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(89, 40)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "Limpiar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextPlaca
+        '
+        Me.TextPlaca.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TextPlaca.Location = New System.Drawing.Point(246, 37)
+        Me.TextPlaca.Name = "TextPlaca"
+        Me.TextPlaca.Size = New System.Drawing.Size(153, 29)
+        Me.TextPlaca.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.White
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI Symbol", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(4, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(217, 37)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Ingrese la placa"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(4, 4)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(0, 17)
+        Me.Label8.TabIndex = 2
+        '
+        'Panel1
+        '
+        Me.Panel1.BackgroundImage = CType(resources.GetObject("Panel1.BackgroundImage"), System.Drawing.Image)
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Controls.Add(Me.Label8)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.TextPlaca)
+        Me.Panel1.Location = New System.Drawing.Point(13, 13)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(434, 96)
+        Me.Panel1.TabIndex = 16
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(459, 474)
+        Me.Controls.Add(Me.Panel4)
+        Me.Controls.Add(Me.TextDevolver)
+        Me.Controls.Add(Me.TextPaga)
+        Me.Controls.Add(Me.TextIva)
+        Me.Controls.Add(Me.TextValor)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Panel2)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "The Parking"
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.Panel4.ResumeLayout(False)
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtSi As CheckBox
+    Friend WithEvents txtNo As CheckBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextValor As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextIva As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextPaga As TextBox
+    Friend WithEvents TextDevolver As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextPlaca As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Panel1 As Panel
+End Class
---
+++ aplicacionTheParking/Form1.vb
@@ -0,0 +1,126 @@
+﻿
+Public Class Form1
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Conexiones.AbrirConexion()
+        Conexiones.Cnn.Open()
+
+        Dim result As Integer
+        Dim fecha As Date
+        Dim porcentaje As Integer
+        Dim Llantas_2 As Integer
+        Dim Llantas_4 As Integer
+        Dim Llantas_8 As Integer
+        Dim recibo As Integer
+        Dim iva As Single
+
+
+        fecha = Now
+        recibo = TextPaga.Text
+        Llantas_2 = 12000
+        Llantas_4 = 18000
+        Llantas_8 = 32000
+        iva = 0.19
+
+
+        result = Llantas_2 + porcentaje
+
+        If result < recibo Then
+
+
+            If (ComboBox1.Text = "2 LLANTAS") And (txtSi.Checked = True) Then
+
+                porcentaje = Llantas_2 * iva
+                result = Llantas_2 + porcentaje
+
+
+                TextIva.Text = porcentaje
+                TextValor.Text = result
+                TextDevolver.Text = result - recibo
+
+            End If
+
+            If (ComboBox1.Text = "2 LLANTAS") And (txtNo.Checked = True) Then
+
+
+                result = Llantas_2
+
+                TextIva.Text = "0"
+                TextValor.Text = result
+                TextDevolver.Text = result - recibo
+
+            End If
+
+            If (ComboBox1.Text = "4 LLANTAS") And (txtSi.Checked = True) Then
+
+                porcentaje = Llantas_4 * iva
+                result = Llantas_4 + porcentaje
+
+                TextIva.Text = porcentaje
+                TextValor.Text = result
+                TextDevolver.Text = result - recibo
+
+            End If
+
+            If (ComboBox1.Text = "4 LLANTAS") And (txtNo.Checked = True) Then
+
+
+                result = Llantas_4
+
+                TextIva.Text = "0"
+                TextValor.Text = result
+                TextDevolver.Text = result - recibo
+
+            End If
+
+            If (ComboBox1.Text = "8 LLANTAS") And (txtSi.Checked = True) Then
+
+                porcentaje = Llantas_8 * iva
+                result = Llantas_8 + porcentaje
+
+                TextIva.Text = porcentaje
+                TextValor.Text = result
+                TextDevolver.Text = result - recibo
+
+            End If
+
+            If (ComboBox1.Text = "8 LLANTAS") And (txtNo.Checked = True) Then
+
+
+                result = Llantas_8
+
+                TextIva.Text = "0"
+                TextValor.Text = result
+                TextDevolver.Text = result - recibo
+
+            End If
+
+            If TextPlaca.Text <> "" Then
+                Dim cmd As New SqlClient.SqlCommand("insert into dbo.aplicativo ( placa, pago_recibo , iva, valor_pagar, devuelta, fecha) values('" & TextPlaca.Text & "','" & TextPaga.Text & "','" & TextIva.Text & "','" & TextValor.Text & "','" & TextDevolver.Text & "','" & fecha & "')", Conexiones.Cnn)
+                cmd.ExecuteNonQuery()
+            End If
+        Else
+
+            MsgBox("El valor ingresado es inferior al cobrado")
+
+        End If
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        TextPaga.Text = ""
+        TextPlaca.Text = ""
+        TextIva.Text = ""
+        TextValor.Text = ""
+        TextDevolver.Text = ""
+        ComboBox1.Text = "Seleccionar"
+        txtSi.Checked = False
+        txtNo.Checked = False
+
+    End Sub
+End Class
